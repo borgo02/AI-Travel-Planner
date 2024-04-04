1a49c66dc388de82141774bd12aa73ac6b7f4a91
Paolo Roselli <paolo.roselli02@gmail.com>
2024-04-04 18:05:52 +0200

    add component file and attrs file

---
+++ app/src/main/java/com/example/aitravelplanner/ui/components/InterestComponent.kt
@@ -0,0 +1,10 @@
+package com.example.aitravelplanner.ui.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class InterestComponent(context: Context, attrs: AttributeSet) : View(context, attrs)
+{
+
+}
