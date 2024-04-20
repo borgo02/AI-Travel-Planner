5ba17ebb016c3ab389df8e995e13ef58aad93266
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-04-20 21:48:00 +0200

    profile fragment created

---
+++ app/src/main/java/com/example/aitravelplanner/ui/profile/ProfileFragment.kt
@@ -0,0 +1,43 @@
+package com.example.aitravelplanner.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aitravelplanner.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val travelCardRecyclerView: RecyclerView = binding.cardTravelRecyclerView
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
