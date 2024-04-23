e0fb231543b8fd72b94ab066330581380f09e726
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-04-23 21:05:09 +0200

    added flags on adapter for shared travels fragment

--- app/src/main/java/com/example/aitravelplanner/CardAdapter.kt
+++ app/src/main/java/com/example/aitravelplanner/CardAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.aitravelplanner
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aitravelplanner.ui.profile.ProfileFragment
+import com.example.aitravelplanner.ui.profile.SharedTravelsFragment
+import org.checkerframework.checker.index.qual.GTENegativeOne
 
 class CardAdapter(private val cards: List<CardTravel>, private val fragment: Fragment) : RecyclerView.Adapter<CardAdapter.CardHolder>() {
     class CardHolder(private val row: View) : RecyclerView.ViewHolder(row) {
@@ -15,6 +18,7 @@ class CardAdapter(private val cards: List<CardTravel>, private val fragment: Fra
         val travelImage: ImageView = row.findViewById(R.id.travelImage)
         val travelName: TextView = row.findViewById(R.id.travelName)
         val affinityPerc: TextView = row.findViewById(R.id.affinityPerc)
+        val affinityImage: ImageView = row.findViewById(R.id.affinityImage)
         val likesNumber: TextView = row.findViewById(R.id.likesNumber)
         val likesImage: ImageView = row.findViewById(R.id.likesImage)
         val shareImage: ImageView = row.findViewById(R.id.shareImage)
@@ -34,32 +38,39 @@ class CardAdapter(private val cards: List<CardTravel>, private val fragment: Fra
 
         val childFragmentManager = fragment.childFragmentManager
         val isProfileFragment = childFragmentManager.findFragmentById(R.id.fragment_profile) is ProfileFragment
-
-        if(isProfileFragment){
-            holder.affinityPerc.visibility = View.GONE
-            holder.likesNumber.visibility = View.GONE
-            holder.likesImage.visibility = View.GONE
-            holder.shareImage.visibility = View.VISIBLE
-            holder.timestamp.visibility = View.VISIBLE
-
-            holder.shareImage.setImageResource(currentCard.shareImage)
-            holder.timestamp.text = currentCard.timestamp
-        }else{
-            holder.affinityPerc.visibility = View.VISIBLE
-            holder.likesNumber.visibility = View.VISIBLE
-            holder.likesImage.visibility = View.VISIBLE
-            holder.shareImage.visibility = View.GONE
-            holder.timestamp.visibility = View.GONE
-
-            holder.affinityPerc.text = currentCard.affinityPerc
-            holder.likesNumber.text = currentCard.likesNumber
-            holder.likesImage.setImageResource(currentCard.likesImage)
-        }
+        val isSharedTravelsProfileFragment = childFragmentManager.findFragmentById(R.id.fragment_shared_travels_profile) is SharedTravelsFragment
 
         holder.username.text = currentCard.username
         holder.userImage.setImageResource(currentCard.userImage)
         holder.travelImage.setImageResource(currentCard.travelImage)
         holder.travelName.text = currentCard.travelName
+
+        holder.affinityPerc.visibility = View.VISIBLE
+        holder.likesNumber.visibility = View.VISIBLE
+        holder.likesImage.visibility = View.VISIBLE
+        holder.shareImage.visibility = View.VISIBLE
+        holder.timestamp.visibility = View.VISIBLE
+
+        if(isProfileFragment){
+            holder.affinityPerc.visibility = View.GONE
+            holder.likesNumber.visibility = View.GONE
+            holder.likesImage.visibility = View.GONE
+
+            holder.shareImage.setImageResource(currentCard.shareImage)
+            holder.timestamp.text = currentCard.timestamp
+        }else{
+            if(isSharedTravelsProfileFragment){
+                holder.affinityPerc.visibility = View.GONE
+            }else{
+                holder.timestamp.visibility = View.GONE
+                holder.affinityImage.setImageResource(currentCard.affinityImage)
+                holder.affinityPerc.text = currentCard.affinityPerc
+            }
+
+            holder.shareImage.visibility = View.GONE
+            holder.likesNumber.text = currentCard.likesNumber
+            holder.likesImage.setImageResource(currentCard.likesImage)
+        }
     }
 
     override fun getItemCount(): Int = cards.size
