43f5960bdd362134f92600d8c3c79a17da4c0f5a
samuele <sacchettisamuele@gmail.com>
2024-04-27 15:45:22 +0200

    View model for Dashboard Fragment

--- app/src/main/java/com/example/aitravelplanner/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/aitravelplanner/ui/dashboard/DashboardFragment.kt
@@ -1,39 +1,30 @@
-package com.example.aitravelplanner.ui.dashboard
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.viewModels
 import com.example.aitravelplanner.databinding.FragmentDashboardBinding
+import com.example.aitravelplanner.ui.dashboard.DashboardViewModel
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val dashboardViewModel: DashboardViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val travelCardRecyclerView: RecyclerView = binding.travelCardsRecyclerView
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        binding.viewmodel = dashboardViewModel
 
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/aitravelplanner/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/aitravelplanner/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,42 @@ package com.example.aitravelplanner.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.aitravelplanner.ui.components.CardTravel
 
 class DashboardViewModel : ViewModel() {
+    private val _cardTravel = MutableLiveData<ArrayList<CardTravel>>()
+    val cardTravel: LiveData<ArrayList<CardTravel>>
+        get() = _cardTravel
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun loadCardTravel(){
+        val cardRoma = CardTravel("Mario Rossi", 1, 2,
+                        "Roma", "82", 2, "1000",
+                        2, 1, "12-04-2024")
+        val cardTeramo = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardMilano = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardTorino = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardAncona = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardVenezia = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardGiulianova = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+        val cardLecce = CardTravel("Mario Rossi", 1, 2,
+            "Roma", "82", 2, "1000",
+            2, 1, "12-04-2024")
+
+        val cards = arrayListOf(cardRoma, cardTeramo, cardMilano, cardTorino,
+                                cardAncona, cardVenezia, cardGiulianova, cardLecce)
+
+        _cardTravel.value = cards
     }
-    val text: LiveData<String> = _text
 }
