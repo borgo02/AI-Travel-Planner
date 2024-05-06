86b62d18aa3a11b8ae515ea73d7544155daaac06
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-05-06 11:12:11 +0200

    -added margin bottom card travel

--- app/src/main/java/com/example/aitravelplanner/ui/components/travelCard/CardAdapter.kt
+++ app/src/main/java/com/example/aitravelplanner/ui/components/travelCard/CardAdapter.kt
@@ -2,8 +2,11 @@ package com.example.aitravelplanner.ui.components.travelCard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.compose.ui.unit.dp
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aitravelplanner.R
@@ -12,7 +15,7 @@ import com.example.aitravelplanner.ui.profile.SharedTravelsFragment
 import com.squareup.picasso.Picasso
 
 class CardAdapter(private val cards: ArrayList<CardTravel>, private val isLiked: ((CardTravel) -> Boolean)? = null, private val fragment: Fragment) : RecyclerView.Adapter<CardAdapter.CardHolder>() {
-    class CardHolder(private val row: View) : RecyclerView.ViewHolder(row) {
+    class CardHolder(val row: View) : RecyclerView.ViewHolder(row) {
         val username: TextView = row.findViewById(R.id.username)
         val userImage: ImageView = row.findViewById(R.id.userImage)
         val travelImage: ImageView = row.findViewById(R.id.travelImage)
@@ -93,6 +96,13 @@ class CardAdapter(private val cards: ArrayList<CardTravel>, private val isLiked:
                     holder.likesNumber.text = currentCard.travelLikes.toString()
                 }
             }
+        }
+
+        if(position == this.itemCount - 1) {
+            val params: FrameLayout.LayoutParams =
+                holder.row.layoutParams as FrameLayout.LayoutParams
+            params.bottomMargin = 100
+            holder.row.layoutParams = params
         }
     }
 
