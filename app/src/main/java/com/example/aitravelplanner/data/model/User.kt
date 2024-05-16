21abcb35806525fddd24c392b356e6d34d9e2972
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-05-16 18:12:00 +0200

    -model classes added

--- app/src/main/java/com/example/aitravelplanner/data/model/User.kt
+++ app/src/main/java/com/example/aitravelplanner/data/model/User.kt
@@ -1,11 +1,12 @@
 package com.example.aitravelplanner.data.model
 
+import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.PropertyName
 
 data class User(
-    @PropertyName("idUser") var idUser: String,
-    @PropertyName("email") var email: String,
-    @PropertyName("firstname") var firstname: String,
-    @PropertyName("lastname") var lastname: String,
-    @PropertyName("interests") var interests: Map<String, Int>
+    @DocumentId var idUser: String?,
+    @PropertyName("email") var email: String?,
+    @PropertyName("fullname") var fullname: String?,
+    @PropertyName("interests") var interests: Map<*, *>,
+    @PropertyName("likedTravels") var likedTravels: ArrayList<Likes>?
 )
