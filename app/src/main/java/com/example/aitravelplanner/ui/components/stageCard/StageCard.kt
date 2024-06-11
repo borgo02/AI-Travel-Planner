50808246243b2371523366f2fd181e5c9a107a44
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-06-11 12:30:05 +0200

    - added comments

--- app/src/main/java/com/example/aitravelplanner/ui/components/stageCard/StageCard.kt
+++ app/src/main/java/com/example/aitravelplanner/ui/components/stageCard/StageCard.kt
@@ -1,4 +1,8 @@
 package com.example.aitravelplanner.ui.components.stageCard
 
-
+/** Data class utile per la visualizzazione delle card delle tappe relative ad un viaggio.
+ *
+ * Utile anche dopo la generazione di un viaggio per aggiungere o eliminare tappe.
+ *
+ */
 data class StageCard (val stageName:String, val stageImage: String, val stageAffinity: Int, var isSearched: Boolean = false, var isSelected: Boolean = false, var description: String = "")
--- app/src/main/java/com/example/aitravelplanner/ui/components/travelCard/CardTravel.kt
+++ app/src/main/java/com/example/aitravelplanner/ui/components/travelCard/CardTravel.kt
@@ -2,6 +2,9 @@ package com.example.aitravelplanner.ui.components.travelCard
 
 import com.example.aitravelplanner.ui.components.stageCard.StageCard
 
+/** Data class utilizzata per la modellazione delle card dei viaggi nella dashboard e nel profilo
+ *
+ */
 data class CardTravel(val username: String, val userImage: String,
                       val travelImage: String, var travelName: String,
                       val affinityPerc: String?, var travelLikes: Int?,
