12c4748ab7920152026bca57e6515b452cc8ca71
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-06-12 19:28:33 +0200

    - fake travels added

--- app/src/test/java/com/example/aitravelplanner/data/repository/travel/TravelRepositoryMock.kt
+++ app/src/test/java/com/example/aitravelplanner/data/repository/travel/TravelRepositoryMock.kt
@@ -3,10 +3,22 @@ package com.example.aitravelplanner.data.repository.travel
 import com.example.aitravelplanner.data.model.Stage
 import com.example.aitravelplanner.data.model.Travel
 import com.example.aitravelplanner.data.model.User
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.PropertyName
 import io.mockk.mockk
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class TravelRepositoryMock : ITravelRepository {
-    private val travels = mutableListOf<Travel>()
+    private val dateString = "10-10-2002"
+    private val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+    private val date: Date? = dateFormat.parse(dateString)
+    private val travel1 = Travel(idTravel = "1", idUser = null, info = "Info", name = "test1", isShared = false, timestamp = date, numberOfLikes = 0, imageUrl = "imageURL", stageList = null, isLiked = false)
+    private val travel2 = Travel(idTravel = "2", idUser = null, info = "Info", name = "Milano", isShared = false, timestamp = date, numberOfLikes = 0, imageUrl = "imageURL", stageList = null, isLiked = true)
+    private val travel3 = Travel(idTravel = "3", idUser = null, info = "Info", name = "New York", isShared = true, timestamp = date, numberOfLikes = 10, imageUrl = "imageURL", stageList = null, isLiked = false)
+    private val travels = mutableListOf(travel1, travel2, travel3)
     private val stages = mutableListOf<Stage>()
 
     override suspend fun setTravel(travel: Travel) {
